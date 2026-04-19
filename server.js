e2211d2377746de5e1aed6b30cd1c90e77650962
Antonio Filho <antoniodollmichael@gmail.com>
2026-04-19 18:34:24 -0300

    fixes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.static("public"));
 const usuarioRoutes = require("./routes/usuario");
 app.use("/", usuarioRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
